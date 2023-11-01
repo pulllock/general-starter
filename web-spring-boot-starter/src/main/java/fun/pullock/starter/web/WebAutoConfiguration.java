a04839bcd44176016d2440ab18460aa6b83b45da
Pullock <pullocked@gmail.com>
2023-11-01 19:21:56 +0800

    update web-spring-boot-starter config

--- web-spring-boot-starter/src/main/java/fun/pullock/starter/web/WebAutoConfiguration.java
+++ web-spring-boot-starter/src/main/java/fun/pullock/starter/web/WebAutoConfiguration.java
@@ -1,12 +1,10 @@
 package fun.pullock.starter.web;
 
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import fun.pullock.starter.web.support.ClientHttpRequestLogInterceptor;
 import fun.pullock.starter.web.support.ClientHttpRequestTraceIdInterceptor;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,28 +13,36 @@ import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Configuration
 @EnableConfigurationProperties(WebProperties.class)
 public class WebAutoConfiguration {
 
-    private static final String LOCAL_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebAutoConfiguration.class);
 
-    @Value("${general.starter.web.logEnable:true}")
-    private boolean restLogEnable;
+    private WebProperties webProperties;
+
+    public WebAutoConfiguration(WebProperties webProperties) {
+        this.webProperties = webProperties;
+    }
 
     @Bean
     @ConditionalOnMissingBean(RestTemplate.class)
     public RestTemplate restTemplate() {
+        LOGGER.info("General RestTemplate enabled");
         // 使用BufferingClientHttpRequestFactory，可以多次重复读取请求体和响应体
         RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
         List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
-        // 添加TraceId到请求头中的拦截器
-        interceptors.add(new ClientHttpRequestTraceIdInterceptor());
 
-        if (restLogEnable) {
+        if (webProperties.getRestTemplate().isTraceEnabled()) {
+            LOGGER.info("General RestTemplate trace id enabled");
+            // 添加TraceId到请求头中的拦截器
+            interceptors.add(new ClientHttpRequestTraceIdInterceptor());
+        }
+
+        if (webProperties.getRestTemplate().isLogEnabled()) {
+            LOGGER.info("General RestTemplate log enabled");
             // RestTemplate请求的日志记录
             interceptors.add(new ClientHttpRequestLogInterceptor());
         }
@@ -45,17 +51,4 @@ public class WebAutoConfiguration {
         return restTemplate;
     }
 
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-        return builder -> {
-            // LocalDateTime格式
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(LOCAL_DATE_TIME_FORMAT);
-
-            // 序列化时LocalDateTime格式由默认的yyyy-MM-ddTHH:mm:ss修改为yyyy-MM-dd HH:mm:ss
-            builder.serializers(new LocalDateTimeSerializer(dateTimeFormatter));
-
-            // 反序列化时LocalDateTime格式由默认的yyyy-MM-ddTHH:mm:ss修改为yyyy-MM-dd HH:mm:ss
-            builder.deserializers(new LocalDateTimeDeserializer(dateTimeFormatter));
-        };
-    }
 }
--- web-spring-boot-starter/src/main/java/fun/pullock/starter/web/WebProperties.java
+++ web-spring-boot-starter/src/main/java/fun/pullock/starter/web/WebProperties.java
@@ -6,16 +6,42 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "general.starter.web")
 public class WebProperties {
 
-    /**
-     * 是否启用日志
-     */
-    private boolean logEnable = true;
+    private RestTemplate restTemplate = new RestTemplate();
 
-    public boolean isLogEnable() {
-        return logEnable;
+    public RestTemplate getRestTemplate() {
+        return restTemplate;
     }
 
-    public void setLogEnable(boolean logEnable) {
-        this.logEnable = logEnable;
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public static class RestTemplate {
+
+        /**
+         * 是否启用RestTemplate日志
+         */
+        private boolean logEnabled = true;
+
+        /**
+         * 使用启用RestTemplate的trace id功能
+         */
+        private boolean traceEnabled = true;
+
+        public boolean isLogEnabled() {
+            return logEnabled;
+        }
+
+        public void setLogEnabled(boolean logEnabled) {
+            this.logEnabled = logEnabled;
+        }
+
+        public boolean isTraceEnabled() {
+            return traceEnabled;
+        }
+
+        public void setTraceEnabled(boolean traceEnabled) {
+            this.traceEnabled = traceEnabled;
+        }
     }
 }
