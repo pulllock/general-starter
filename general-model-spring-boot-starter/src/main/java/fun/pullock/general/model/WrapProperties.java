cd4f5cd82f8542d9fe18825eac68b61f5cb2a170
Pullock <pullocked@gmail.com>
2023-11-01 17:56:45 +0800

    update general-model-spring-boot-starter config

--- general-model-spring-boot-starter/src/main/java/fun/pullock/general/model/WrapProperties.java
+++ general-model-spring-boot-starter/src/main/java/fun/pullock/general/model/WrapProperties.java
@@ -5,29 +5,4 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "general.starter.wrap")
 public class WrapProperties {
 
-    /**
-     * 是否启用结果包装，默认启用
-     */
-    private boolean result = true;
-
-    /**
-     * 是否启用结果包装，默认启用
-     */
-    private boolean exception = true;
-
-    public boolean isResult() {
-        return result;
-    }
-
-    public void setResult(boolean result) {
-        this.result = result;
-    }
-
-    public boolean isException() {
-        return exception;
-    }
-
-    public void setException(boolean exception) {
-        this.exception = exception;
-    }
 }
